33289220229ed4192bdcb370685f9ff8ee98d659
jotano <jotanosanm@educacion.navarra.es>
2023-10-27 00:49:54 +0200

    Ejercicio9

---
+++ Ejercicio9.py
@@ -0,0 +1,5 @@
+numero = int(input("Escribe el número de filas que quieres que tenga el triángulo:"))
+for i in range(1, numero *2, 2):
+        print(i, end="")
+        print()
+input("Pulsa enter para salir")

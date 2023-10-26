b41cd59d37812f717830a644bac531216e3237a4
jotano <jotanosanm@educacion.navarra.es>
2023-10-27 00:48:29 +0200

    Ejercicio7

---
+++ Ejercicio7.py
@@ -0,0 +1,4 @@
+tamaño_triangulo= int(input("Introduce un número que determine el tamaño del triángulo:"))
+for i in range(1, tamaño_triangulo + 1):
+    print("*" * i)
+input("Pulsa enter para salir")

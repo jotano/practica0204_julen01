f3815025e225f771555258bc906dfe6d71917055
jotano <jotanosanm@educacion.navarra.es>
2023-10-27 00:49:12 +0200

    Ejercicio8

---
+++ Ejercicio8.py
@@ -0,0 +1,7 @@
+for i in range(1, 11):
+    print(i)
+    for a in range(1, 11):
+        tabla_multiplicar = i * a
+        print(i, "*", a, "=", tabla_multiplicar)
+    print()  
+input("Pulsa enter para salir")  

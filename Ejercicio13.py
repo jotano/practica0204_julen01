a2b96c537e7cd5845df00492b44852a5ab818954
jotano <jotanosanm@educacion.navarra.es>
2023-10-27 00:55:08 +0200

    Ejercicio13

---
+++ Ejercicio13.py
@@ -0,0 +1,6 @@
+while True:
+    entrada_y_salida= input("Escribe todo lo que quieras recibir de vuelta o escribe salir para acabar el programa:")
+    if entrada_y_salida.lower() == "salir":
+        break
+    else:
+        print(entrada_y_salida)
